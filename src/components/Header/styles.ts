d8077155d6726300fa383fb7510f4ba9d3874441
gustavo.bastos@pti.org.br <gustavo.bastos@pti.org.br>
2022-12-14 09:35:18 -0300

    tela de turmas finalizadas

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-
+import { CaretLeft } from 'phosphor-react-native';
 
 export const Container = styled.View`
   width: 100%;
@@ -7,10 +7,21 @@ export const Container = styled.View`
   align-items: center;
   margin-top: 12px;
   justify-content: center;
-  `
+  `;
 
   export const Logo = styled.Image`
   width: 46px;
   height: 55px;
   
-  `
+  `;
+
+export const BackButton = styled.TouchableOpacity`
+flex: 1;
+`;
+
+  export const BackIcon = styled(CaretLeft).attrs(({theme})=>({
+    color: theme.COLORS.WHITE,
+    size: 32,
+  }))`
+  
+  `;
